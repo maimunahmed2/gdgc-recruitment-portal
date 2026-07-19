57489239aa7b5fa1c99ba224b409cf7e044995c9
Maimun Ahmed <studywithmaimun@gmail.com>
2026-07-19 12:30:30 +0530

    addded admin registration feat

--- src/routes/api.old/debug/emails/+server.ts
+++
@@ -1,6 +0,0 @@
-import { json, type RequestHandler } from '@sveltejs/kit';
-import { readEmails } from '$lib/server/db';
-
-export const GET: RequestHandler = async () => {
-  return json({ emails: readEmails() });
-};
